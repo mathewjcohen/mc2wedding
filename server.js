3a7621ce26da161be28da2f06f0543e0f39a2499
Mathew Cohen <mathewjcohen@gmail.com>
2018-01-22 22:40:31 -0800

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+
+const app = express();
+
+app.use(compression());
+app.use(express.static(__dirname + "/client/static"));
+
+app.set("views", path.join(__dirname, "./client/views"));
+app.set('view engine', 'ejs');
+
+var routes_setter = require('./server/config/routes.js');
+routes_setter(app);
+
+app.listen(8010);
